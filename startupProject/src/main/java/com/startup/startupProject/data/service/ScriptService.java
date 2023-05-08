71a67fa6e595a1f59ea5437edb901d523645b9b4
ikk5515 <ikk5515@naver.com>
2023-05-08 23:44:50 +0900

    일부 어노테이션 수정

--- startupProject/src/main/java/com/startup/startupProject/data/service/ScriptService.java
+++ startupProject/src/main/java/com/startup/startupProject/data/service/ScriptService.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class ScriptService implements ScriptMapper {
 
