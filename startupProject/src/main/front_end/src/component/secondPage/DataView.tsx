a27e537f09e9fb387699ff4060797b3e2cfa18f2
gyojinK <gyojink@gyojinKui-MacBookAir.local>
2023-05-10 01:18:38 +0900

    Fix style & Add hover state, xIcon

--- startupProject/src/main/front_end/src/component/secondPage/DataView.tsx
+++ startupProject/src/main/front_end/src/component/secondPage/DataView.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './DataView.css';
 import Recorder from './Recorder';
+import {Simulate} from "react-dom/test-utils";
+import click = Simulate.click;
 
-export default function DataView(){
+export default function DataView(props:{scClick:number[], ChangeScClick:any, data:{ id?: number, script?: string }[]}){
+    const [hover, setHover] = useState(0);
+    const [xclick, setxclick] = useState(0);
 
+    const handleXicon = () => {
+        const value = hover ? 0 : 1;
+        setHover(value);
+    }
+
+    const xStyle = () => {
+        let style = {
+            transition : '0.5s',
+            backgroundColor : hover ? 'black' : 'white',
+        }
+        return style;
+    }
     return(
         <div className='viewWrap'>
             <div className='view'>
+                <div className='xWrap' onMouseOver={handleXicon} onMouseDown={handleXicon} onClick={props.ChangeScClick}>
+                    <span className='x1' style={xStyle()}></span>
+                    <span className='x2' style={xStyle()}></span>
+                </div>
                 <Recorder/>
             </div>
         </div>
