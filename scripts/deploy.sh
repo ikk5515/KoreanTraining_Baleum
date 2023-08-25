1a73ec8fd6e39285b2287d27c648be4ab9e91d22
ikk5515 <ikk5515@naver.com>
2023-08-26 02:26:57 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,22 @@
-#!/bin/bash
-BUILD_JAR=$(ls /home/ikk5515/app/KoreanTraining_Baleum/startupProject/build/libs/startupProject-0.0.1-SNAPSHOT.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ikk5515/app/KoreanTraining_Baleum/deploy.log
+#!/usr/bin/env bash
 
-echo ">>> build 파일 복사" >> /home/ikk5515/app/KoreanTraining_Baleum/deploy.log
-DEPLOY_PATH=/home/ikk5515/app/KoreanTraining_Baleum
-cp $BUILD_JAR $DEPLOY_PATH
+REPOSITORY=/home/ikk5515/app/KoreanTraining_Baleum
+cd $REPOSITORY
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ikk5515/app/KoreanTraining_Baleum/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+APP_NAME=startupProject-0.0.1-SNAPSHOT.jar
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ikk5515/app/KoreanTraining_Baleum/deploy.log
+  echo "> 종료할 애플리케이션이 없습니다."
 else
-  echo ">>> kill -9 $CURRENT_PID"
+  echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo ">>> DEPLOY_JAR 배포"    >> /home/ikk5515/app/KoreanTraining_Baleum/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ikk5515/deploy.log 2>/home/ikk5515/app/KoreanTraining_Baleum/deploy_err.log &
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /home/ikk5515/deploy.log 2> /home/ikk5515/app/KoreanTraining_Baleum/deploy_err.log < /home/ikk5515/deploy.log &
