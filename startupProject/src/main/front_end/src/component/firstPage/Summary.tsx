69204602005c992e7c4700e0eaa68af5f6ab1b7c
ikk5515 <ikk5515@naver.com>
2023-08-26 04:50:40 +0900

    CI/CD 테스트 완료

--- startupProject/src/main/front_end/src/component/firstPage/Summary.tsx
+++ startupProject/src/main/front_end/src/component/firstPage/Summary.tsx
@@ -18,7 +18,7 @@ export default function Summary(){
                         <p>LEARN</p>
                         <p>YOUR</p>
                         <p>CONFIDENT</p>
-                        <p>CI/CD TEST</p>
+                        <p>PRONUNCIATION</p>
                     </div>
                 </div>
                 <BgElement height={''} display={''}/>
