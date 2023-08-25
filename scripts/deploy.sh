ddf44564b3d9c84d57b00d83a336fd74b3e3822f
ikk5515 <ikk5515@naver.com>
2023-08-26 01:11:03 +0900

    배포 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ JAR_NAME=$(basename $BUILD_JAR)
 echo ">>> build 파일명: $JAR_NAME" >> /home/ikk5515/app/KoreanTraining_Baleum/deploy.log
 
 echo ">>> build 파일 복사" >> /home/ikk5515/app/KoreanTraining_Baleum/deploy.log
-DEPLOY_PATH=/home/ikk5515/app/KoreanTraining_Baleum/
+DEPLOY_PATH=/home/ikk5515/app/KoreanTraining_Baleum
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ikk5515/app/KoreanTraining_Baleum/deploy.log
@@ -15,7 +15,7 @@ then
   echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ikk5515/app/KoreanTraining_Baleum/deploy.log
 else
   echo ">>> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
